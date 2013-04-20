c13e19b099675ba31031e3be86d0adea2d68cb05
Mikołaj Stolarski <grimor.mc@gmail.com>
2013-04-20 12:11:07 +0200

    Responsive menu

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 (function() {
-
+	var menu = document.getElementById('menu');
+	menu.getElementsByClassName('menu-button')[0].addEventListener('click', function(e) {
+		e.preventDefault();
+		var list = menu.getElementsByTagName('ul')[0];
+		if (list.style.display == 'block') {
+			list.style.display = '';
+		} else {
+			list.style.display = 'block';
+		}
+	})
 })();
